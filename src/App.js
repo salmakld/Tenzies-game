e735f5ae3deaebebc846db33a9d3d72619bc8d38
salmakld <skhouladisalma@gmail.com>
2022-06-02 21:50:59 +0200

    update states

--- src/App.js
+++ src/App.js
@@ -2,66 +2,66 @@ import React from 'react';
 import './App.css';
 import Die from './Components/Die';
 import Confetti from 'react-confetti'
+import {nanoid} from "nanoid"
+
 
 function App() {
   // Helper functions
   function RandomInt(){
-    // Generate random number from 0 to 9
+    // Generate random number from 1 to 6 inclusive
     return Math.ceil(Math.random() * 6)
   }
-  function initTenzies(array){
-    // Initilize the array values of the state tenziesArray
+  function initTenzies(){
     // Generate random number only when value wasn't clicked
+    const array = []
     for (let i=0; i<10; i++){
-      array[i] = RandomInt()
+      array[i] = {
+        "id": nanoid(),
+        "value": RandomInt(), 
+        "chosen": false,
+      }
     }
     return array
   }
 
-  function handleDiceClick(index){
-    // Update array chosen of the state tenziesArray
-    // If die is clicked switch its value to true
-    const newTenziesArray = [...tenziesArray.chosen];
-    newTenziesArray[index] = !newTenziesArray[index];
-    setTenziesArray(oldTenziesArray => (
-      {...oldTenziesArray,
-      "chosen": newTenziesArray
+  function handleDiceClick(id){
+    // If die is clicked switch its value
+    const newTenziesArray = [...tenziesArray];
+    for (let i=0; i<newTenziesArray.length; i++){
+      if (newTenziesArray[i].id === id){
+        newTenziesArray[i].chosen = !newTenziesArray[i].chosen
       }
-    ))}
+    }
+    setTenziesArray(newTenziesArray)
+    }
 
   function handleRoll(){
-    // Update array values of the state tenziesArray
     // Generate random values except for the dice already clicked
-    const newTenziesArray = [...tenziesArray.values];
+    const newTenziesArray = [...tenziesArray];
     for (let i=0; i<10; i++){
-      if (!tenziesArray.chosen[i]){
-        newTenziesArray[i] = RandomInt()
+      if (!tenziesArray[i].chosen){
+        newTenziesArray[i].value = RandomInt()
       }
     } 
-    setTenziesArray(oldTenziesArray => ({
-      ...oldTenziesArray,
-      "values": newTenziesArray,
-      "numberClicks": oldTenziesArray.numberClicks + 1
-  }))}
+    setTenziesArray(newTenziesArray)
+    setNumberClicks(oldNumberClicks => oldNumberClicks+1)
+  }
   function replay(){
-    // Reload the page to replay
-    return window.location.reload()
+    // Reset states
+    setTenziesArray(initTenzies())
+    setNumberClicks(0)
   }
   // End helper functions
-  const initTenziesArray = initTenzies([])
-  const initChoiceArray = Array(10).fill(false)
-  const [tenziesArray, setTenziesArray] = React.useState({"values": initTenziesArray, 
-                                                          "chosen": initChoiceArray,
-                                                          "numberClicks": 0})
-  
-  const arrayEelements = tenziesArray.values.map(
-    (value, index) => <Die key={index} 
-                           clicked={tenziesArray.chosen[index]}
-                           value={value}
-                           handleDiceClick={() => handleDiceClick(index)}/>
+  const [tenziesArray, setTenziesArray] = React.useState(initTenzies())
+  const [numberClicks, setNumberClicks] = React.useState(0)
+  const arrayEelements = tenziesArray.map(
+                          die => <Die key={die.id} 
+                                      clicked={die.chosen}
+                                      value={die.value}
+                                      handleDiceClick={() => handleDiceClick(die.id)}/>
   )
-
-  const set = new Set(tenziesArray.chosen)
+  let chosenArray = tenziesArray.map(die => die.chosen)
+  const set = new Set(chosenArray)
 
   return (
     <div className="app">
@@ -70,12 +70,13 @@ function App() {
           Tenzies
         </h1>
         {
-          (set.size === 1 && tenziesArray.chosen[0]) 
+          (set.size === 1 && chosenArray[0]) 
           ? 
+            // All dice hame the same value
             <div>
               <Confetti/>
               <p className='game-description'>
-                Congratulation! You won this game using {tenziesArray.numberClicks} clicks.
+                Congratulation! You won this game using {numberClicks} clicks.
               </p>
               <div className='grid-container'>
                 <div className="grid vertical-center">
@@ -87,6 +88,7 @@ function App() {
             </div>
             
           :
+            // Not all dice have the same value
             <div>
               <p className='game-description'>
                 Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
@@ -96,7 +98,7 @@ function App() {
                   {arrayEelements}
                 </div>
               </div>
-              <p className='clicks'>Number of clicks: {tenziesArray.numberClicks} </p>
+              <p className='clicks'>Number of clicks: {numberClicks} </p>
               <button onClick={handleRoll} className='roll'>Roll</button>
             </div>
         }
